70c288b1f28818729a0cc7c218fdf770908495d7
Toshiki <t.kabu.03@gmail.com>
2021-08-26 21:29:05 +0900

    新規作成画面作成（途中）

--- components/create/InputArea.tsx
+++ components/create/InputArea.tsx
@@ -1,9 +1,49 @@
-const InputArea: React.FC = () => {
+import { Dispatch, SetStateAction } from "react";
+
+type Props = {
+  subject: string;
+  setSubject: Dispatch<SetStateAction<string>>;
+  period: number;
+  setPeriod: Dispatch<SetStateAction<number>>;
+  day: string;
+  setDay: Dispatch<SetStateAction<string>>;
+  addSubject: any;
+};
+
+const InputArea = (props: Props) => {
+  const { subject, setSubject, period, setPeriod, day, setDay, addSubject } =
+    props;
   return (
     <>
-      <div>インプットエリア</div>
-      <div>インプットエリア</div>
-      <div>インプットエリア</div>
+      <select name="day" onChange={(e) => setDay(e.target.value)}>
+        <option>曜日</option>
+        <option value="Sun">日曜</option>
+        <option value="Mon">月曜</option>
+        <option value="Tue">火曜</option>
+        <option value="Wed">水曜</option>
+        <option value="Thu">木曜</option>
+        <option value="Fri">金曜</option>
+        <option value="Sat">土曜</option>
+      </select>
+      <select name="period" onChange={(e) => setPeriod(Number(e.target.value))}>
+        <option>時限</option>
+        <option value="1">1限目</option>
+        <option value="2">2限目</option>
+        <option value="3">3限目</option>
+        <option value="4">4限目</option>
+        <option value="5">5限目</option>
+        <option value="6">6限目</option>
+      </select>
+      <input
+        autoFocus
+        onChange={(e) => setSubject(e.target.value)}
+        placeholder="教科名"
+        type="text"
+        value={subject}
+      />
+      <button onClick={addSubject} disabled={!subject || !period || !day}>
+        追加
+      </button>
     </>
   );
 };
--- components/create/SubjectCreator.tsx
+++ components/create/SubjectCreator.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { NewSubjectProps } from "../../pages/create";
 import Subject from "../Subject";
 import NewSubject from "./NewSubject";
+import InputArea from "./InputArea";
 
 type Props = {
   newSubjects: NewSubjectProps[];
@@ -45,51 +46,34 @@ const SubjectCreator = (props: Props) => {
     setNewSubjects(newList);
   };
 
+  const newSubjectsView = newSubjects.map((subject, index) => {
+    return (
+      <>
+        <div key={index}>
+          <NewSubject
+            subject={subject}
+            deleteSubject={() => deleteSubject(index)}
+          />
+        </div>
+      </>
+    );
+  });
+
   return (
     <>
       <button onClick={submitToParent}>完成</button>
-      <select name="day" onChange={(e) => setDay(e.target.value)}>
-        <option>曜日</option>
-        <option value="Sun">日曜</option>
-        <option value="Mon">月曜</option>
-        <option value="Tue">火曜</option>
-        <option value="Wed">水曜</option>
-        <option value="Thu">木曜</option>
-        <option value="Fri">金曜</option>
-        <option value="Sat">土曜</option>
-      </select>
-      <select name="period" onChange={(e) => setPeriod(Number(e.target.value))}>
-        <option>時限</option>
-        <option value="1">1限目</option>
-        <option value="2">2限目</option>
-        <option value="3">3限目</option>
-        <option value="4">4限目</option>
-        <option value="5">5限目</option>
-        <option value="6">6限目</option>
-      </select>
-      <input
-        autoFocus
-        onChange={(e) => setSubject(e.target.value)}
-        placeholder="教科名"
-        type="text"
-        value={subject}
-      />
-      <button onClick={addSubject} disabled={!subject || !period || !day}>
-        追加
-      </button>
 
-      {newSubjects.map((subject, index) => {
-        return (
-          <>
-            <div key={index}>
-              <NewSubject
-                subject={subject}
-                deleteSubject={() => deleteSubject(index)}
-              />
-            </div>
-          </>
-        );
-      })}
+      <InputArea
+        subject={subject}
+        setSubject={setSubject}
+        period={period}
+        setPeriod={setPeriod}
+        day={day}
+        setDay={setDay}
+        addSubject={addSubject}
+      />
+
+      {newSubjectsView}
     </>
   );
 };
