113eae44e7789e3a3f4199dd34c081dfbf7af1f7
Toshiki <t.kabu.03@gmail.com>
2021-08-26 17:01:34 +0900

    新規作成画面作成（途中）

--- components/create/NewSubject.tsx
+++ components/create/NewSubject.tsx
@@ -1,19 +1,45 @@
 import React from "react";
 import Router from "next/router";
 import { NewSubjectProps } from "../../pages/create";
+import styled from "styled-components";
 
 const NewSubject: React.FC<NewSubjectProps> = (props) => {
   const { subject, week, period, day } = props.newSubject;
   return (
     <>
-      <div>
-        <div>{subject}</div>
-        <div>{week}</div>
-        <div>{period}</div>
-        <div>{day}</div>
-      </div>
+      <Scontainer>
+        <Sitem>{day}</Sitem>
+        <Sitem>{period}</Sitem>
+        <Sitem>{subject}</Sitem>
+        <Sbutton>×</Sbutton>
+      </Scontainer>
     </>
   );
 };
 
 export default NewSubject;
+
+const Scontainer = styled.div`
+  display: flex;
+  background-color: #cbe4ff;
+  width: 300px;
+  margin: 10px;
+  border-radius: 3px;
+`;
+
+const Sitem = styled.div`
+  font-size: 18px;
+  padding: 10px 0;
+  margin-left: 20px;
+`;
+const Sbutton = styled(Sitem)`
+  font-size: 150%;
+  display: inline;
+  color: white;
+  padding: 0;
+  margin: auto 10px 10px auto;
+
+  &hover {
+    cursor: pointer;
+  }
+`;
--- components/create/SubjectCreator.tsx
+++ components/create/SubjectCreator.tsx
@@ -15,7 +15,6 @@ const SubjectCreator: React.FC<Props> = (props) => {
   const [subject, setSubject] = useState("");
   const [week, setWeek] = useState(0);
   const [period, setPeriod] = useState(Number);
-
   const [day, setDay] = useState("");
 
   //並べ替えてから親のコンポーネントに渡す関数
@@ -41,35 +40,35 @@ const SubjectCreator: React.FC<Props> = (props) => {
   return (
     <>
       <button onClick={submitToParent}>完成</button>
-      <button onClick={addSubject} disabled={!subject || !period || !day}>
-        追加
-      </button>
-
+      <select name="day" onChange={(e) => setDay(e.target.value)}>
+        <option>曜日</option>
+        <option value="Sun">日曜</option>
+        <option value="Mon">月曜</option>
+        <option value="Tue">火曜</option>
+        <option value="Wed">水曜</option>
+        <option value="Thu">木曜</option>
+        <option value="Fri">金曜</option>
+        <option value="Sat">土曜</option>
+      </select>
+      <select name="period" onChange={(e) => setPeriod(Number(e.target.value))}>
+        <option>時限</option>
+        <option value="1">1限目</option>
+        <option value="2">2限目</option>
+        <option value="3">3限目</option>
+        <option value="4">4限目</option>
+        <option value="5">5限目</option>
+        <option value="6">6限目</option>
+      </select>
       <input
         autoFocus
         onChange={(e) => setSubject(e.target.value)}
-        placeholder="subject"
+        placeholder="教科名"
         type="text"
         value={subject}
       />
-      <select name="period" onChange={(e) => setPeriod(Number(e.target.value))}>
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-        <option>6</option>
-      </select>
-
-      <select name="day" onChange={(e) => setDay(e.target.value)}>
-        <option>Sunday</option>
-        <option>Monday</option>
-        <option>Tuesday</option>
-        <option>Wednesday</option>
-        <option>Thursday</option>
-        <option>Friday</option>
-        <option>Saturday</option>
-      </select>
+      <button onClick={addSubject} disabled={!subject || !period || !day}>
+        追加
+      </button>
 
       {newSubjects.map((subject, index) => {
         return (
