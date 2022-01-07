02fce1a8b4b5a4a09d2e93bf76d882ec44cb74f6
t-kabu <t.kabu.03@outlook.jp>
2022-01-07 18:08:30 +0900

    バグ修正

--- pages/api/update/subject.ts
+++ pages/api/update/subject.ts
@@ -2,7 +2,10 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/client'
 import prisma from '../../../lib/prisma'
 
-export default async function (req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const session = await getSession({ req })
   if (!session) {
     res.status(401).json({ message: 'Not authenticated' })
