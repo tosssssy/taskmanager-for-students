a034052403a6a9c545929c55e86f8f1c33fdafd8
t-kabu <t.kabu.03@gmail.com>
2022-01-21 12:05:03 +0900

    アクセス時に自動ログインした後の画面遷移を修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { GetServerSideProps } from 'next'
-import { getSession } from 'next-auth/client'
-
+import { useSession, getSession } from 'next-auth/client'
 import Head from 'next/head'
+import Router from 'next/router'
 import React, { FC } from 'react'
 import { Layout } from '../components/Layout'
 import { Welcome } from '../components/top/Welcome'
 const TopPage: FC = () => {
+  const [session] = useSession()
+  if (session) Router.push('/schedule')
+
   return (
     <Layout>
       <Head>
