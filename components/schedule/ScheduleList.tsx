b3abf3f4e2b89773277625ccdbe0621669ea51f5
t-kabu <t.kabu.03@gmail.com>
2022-04-24 22:52:27 +0900

    refactor(schedule): ロジックをhooksに切り出し

--- components/schedule/ScheduleList.tsx
+++ components/schedule/ScheduleList.tsx
@@ -2,40 +2,31 @@
 import { Box, Flex } from '@chakra-ui/react'
 import dayjs from 'dayjs'
 import isBetween from 'dayjs/plugin/isBetween'
-import React, { FC, useMemo } from 'react'
+import React, { FC } from 'react'
+import { useSchedule } from '../../hooks/useSchedule'
 import { SubjectType } from '../../lib/types'
 import { Pagination } from './Pagination'
 import { Subject } from './Subject'
-import { setDateList } from './setDateList'
-import { setStartAndEnd } from './setStartAndEnd'
-import { useSetViewRange } from './useSetViewRange'
 
 // DBから取得できる日付は'2022-01-09T00:00:00.000Z'というフォーマット
 // タイムゾーン周りでずれが生じる可能性があるのでdayjs(2022-01-09)として扱っている
+dayjs.extend(isBetween)
+const Day = ['日', '月', '火', '水', '木', '金', '土']
 
 type Props = {
   subjects: SubjectType[]
 }
 
 export const ScheduleList: FC<Props> = ({ subjects }) => {
-  dayjs.extend(isBetween)
-  const Day = ['日', '月', '火', '水', '木', '金', '土']
-
-  // subjectsの最初と最後の日付のDayjsオブジェクト
-  const { startDate, endDate } = useMemo(
-    () => setStartAndEnd(subjects),
-    [subjects]
-  )
-
-  // 全体の日付(YYYY-MM-DD)の配列
-  const dateList = useMemo(
-    () => setDateList(startDate, endDate),
-    [startDate, endDate]
-  )
-
-  // 表示範囲と今何週目を表示しているか
-  const { firstViewDate, lastViewDate, currentWeekNum, setCurrentWeekNum } =
-    useSetViewRange()
+  const {
+    startDate,
+    endDate,
+    dateList,
+    firstViewDate,
+    lastViewDate,
+    currentWeekNum,
+    setCurrentWeekNum,
+  } = useSchedule(subjects)
 
   return (
     <>
