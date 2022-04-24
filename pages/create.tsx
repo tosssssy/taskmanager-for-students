74ad911b04e77d766eccfdb2f864ba5ea0705c57
t-kabu <t.kabu.03@gmail.com>
2022-04-24 19:53:43 +0900

    fix(pages): ベストプラクティスに近づける

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,7 +1,8 @@
 import { Box } from '@chakra-ui/react'
+import { NextPage } from 'next'
 import Head from 'next/head'
 import Router from 'next/router'
-import React, { FC, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Layout } from '../components/Layout'
 import { Loading } from '../components/Loading'
 import { DateListCreator } from '../components/create/DateListCreator'
@@ -10,17 +11,17 @@ import { NewSubjectType } from '../lib/types'
 import { CreateButton } from './../components/create/CreateButton'
 import { SubjectCreator } from './../components/create/SubjectCreator'
 
-const CreatePage: FC = () => {
-  const [newSubjects, setNewSubjects] = useState<Array<NewSubjectType>>([])
-  const [dateList, setDateList] = useState<Array<Date>>([])
+const CreatePage: NextPage = () => {
+  const [newSubjects, setNewSubjects] = useState<NewSubjectType[]>([])
+  const [dateList, setDateList] = useState<Date[]>([])
 
   const [loading, setLoading] = useState<boolean>(false)
 
   // ユーザーの全データを削除してから新規作成
-  const createNewSchedule = async () => {
+  const createNewSchedule = useCallback(async () => {
     setLoading(true)
     try {
-      await fetch('api/delete', { method: 'DELETE' })
+      await fetch('/api/delete', { method: 'DELETE' })
 
       const body = createNewScheduleList(dateList, newSubjects)
 
@@ -33,26 +34,25 @@ const CreatePage: FC = () => {
       console.error(error)
     }
     Router.push('/schedule')
-  }
-  if (loading) return <Loading />
+  }, [dateList, newSubjects])
 
+  if (loading) return <Loading />
   return (
-    <Layout>
+    <>
       <Head>
         <title>Task Manager | 新規作成</title>
       </Head>
 
-      <Box h={200} />
-
-      <DateListCreator setDateList={setDateList} />
-
-      <SubjectCreator
-        newSubjects={newSubjects}
-        setNewSubjects={setNewSubjects}
-      />
-
-      <CreateButton onclick={createNewSchedule} />
-    </Layout>
+      <Layout>
+        <Box h={200} />
+        <DateListCreator setDateList={setDateList} />
+        <SubjectCreator
+          newSubjects={newSubjects}
+          setNewSubjects={setNewSubjects}
+        />
+        <CreateButton onclick={createNewSchedule} />
+      </Layout>
+    </>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,25 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { GetServerSideProps } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 import { useSession, getSession } from 'next-auth/client'
 import Head from 'next/head'
 import Router from 'next/router'
-import React, { FC } from 'react'
+import React from 'react'
 import { Layout } from '../components/Layout'
 import { Welcome } from '../components/top/Welcome'
-const TopPage: FC = () => {
+
+const TopPage: NextPage = () => {
   const [session] = useSession()
   if (session) Router.push('/schedule')
 
   return (
-    <Layout>
+    <>
       <Head>
         <title>Task Manager</title>
       </Head>
-      <Welcome />
-    </Layout>
+      <Layout>
+        <Welcome />
+      </Layout>
+    </>
   )
 }
 
