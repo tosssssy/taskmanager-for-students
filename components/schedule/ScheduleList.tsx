74a1725864f589b7e080bb477eecc39ce46e5a8f
t-kabu <t.kabu.03@gmail.com>
2022-05-02 22:01:39 +0900

    refactor(ScheduleList): ロジックの切り出しとリファクタリング

--- components/schedule/ScheduleList.tsx
+++ components/schedule/ScheduleList.tsx
@@ -1,55 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Box, BoxProps, Flex, Text, Wrap } from '@chakra-ui/react'
 import dayjs from 'dayjs'
-import isBetween from 'dayjs/plugin/isBetween'
-import React, { FC, useCallback, useEffect, useState } from 'react'
-import { useGetApi } from '../../hooks/useApi'
+import React, { FC } from 'react'
 import { useSchedule } from '../../hooks/useSchedule'
-import { SubjectType, UpdateSubjectType } from '../../types/subject'
-import { putApi } from '../../utils/api'
+import { useSubjectsWithCache } from '../../hooks/useSubjectsWithCache'
+import { SubjectType } from '../../types/subject'
 import { Pagination } from './Pagination'
 import { Subject } from './Subject'
 
 // DBから取得できる日付は'2022-01-09T00:00:00.000Z'というフォーマット
 // タイムゾーン周りでずれが生じる可能性があるのでdayjs(2022-01-09)として扱っている
-dayjs.extend(isBetween)
-const Day = ['日', '月', '火', '水', '木', '金', '土']
+const DayName = ['日', '月', '火', '水', '木', '金', '土']
 
 type Props = {
   subjects: SubjectType[]
 } & BoxProps
 
 export const ScheduleList: FC<Props> = ({ subjects: initSubjects, ...rest }) => {
-  const [subjects, setSubjects] = useState<SubjectType[]>(initSubjects)
   const { dateList, currentWeekNum, setCurrentWeekNum } = useSchedule()
-  const { data: currentWeekData, mutate } = useGetApi<SubjectType[]>(
-    `api/subjects?page=${currentWeekNum}`
-  )
-
-  useEffect(() => {
-    setSubjects((postData) => {
-      if (!currentWeekData) return postData
-
-      const merged = [...postData, ...currentWeekData]
-      const unique = Array.from(new Map(merged.map((v) => [v.id, v])).values())
-      return unique
-    })
-  }, [currentWeekData])
-
-  const updateSubject = useCallback(async (newSubject: UpdateSubjectType) => {
-    try {
-      await putApi('/api/subjects', newSubject)
-      await mutate((postData) =>
-        postData?.map((v) => (v.id === newSubject.id ? { ...v, ...newSubject } : v))
-      )
-    } catch (error) {
-      console.error(error)
-    }
-  }, [])
-
-  // プリフェッチしてキャッシュを保持しておく
-  useGetApi<SubjectType[]>(`api/subjects?page=${currentWeekNum - 1}`)
-  useGetApi<SubjectType[]>(`api/subjects?page=${currentWeekNum + 1}`)
+  const { subjects, updateSubject } = useSubjectsWithCache(currentWeekNum, initSubjects)
 
   return (
     <Box {...rest}>
@@ -60,6 +29,7 @@ export const ScheduleList: FC<Props> = ({ subjects: initSubjects, ...rest }) => 
       <Flex direction={'column'} gap={5} p={25} mx={'auto'} mb={'150px'} maxW={'840px'}>
         {dateList.map((oneDay) => {
           const oneDayjs = dayjs(oneDay)
+          const day = oneDayjs.day()
           const oneDaySubjects = subjects
             .filter((v) => oneDayjs.format('YYYY-MM-DD') === String(v?.date).slice(0, 10))
             .sort((a, b) => (a.period > b.period ? 1 : -1))
@@ -69,13 +39,9 @@ export const ScheduleList: FC<Props> = ({ subjects: initSubjects, ...rest }) => 
               <Flex fontSize={18}>
                 {oneDayjs.format('M/D')}
                 {'('}
-                {Day[oneDayjs.day()] === '土' && (
-                  <Text color='blue.400'>{Day[oneDayjs.day()]}</Text>
-                )}
-                {Day[oneDayjs.day()] === '日' && <Text color='red.400'>{Day[oneDayjs.day()]}</Text>}
-                {Day[oneDayjs.day()] !== '土' && Day[oneDayjs.day()] !== '日' && (
-                  <Text>{Day[oneDayjs.day()]}</Text>
-                )}
+                {DayName[day] === '土' && <Text color='blue.400'>{DayName[day]}</Text>}
+                {DayName[day] === '日' && <Text color='red.400'>{DayName[day]}</Text>}
+                {DayName[day] !== '土' && DayName[day] !== '日' && <Text>{DayName[day]}</Text>}
                 {')'}
               </Flex>
 
---
+++ hooks/useSubjectsWithCache.ts
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useState } from 'react'
+import { SubjectType, UpdateSubjectType } from '../types/subject'
+import { putApi } from '../utils/api'
+import { useGetApi } from './useApi'
+
+export const useSubjectsWithCache = (currentWeekNum: number, initSubjects: SubjectType[]) => {
+  const [subjects, setSubjects] = useState<SubjectType[]>(initSubjects)
+  const { data: currentWeekData, mutate } = useGetApi<SubjectType[]>(
+    `api/subjects?page=${currentWeekNum}`
+  )
+
+  useEffect(() => {
+    setSubjects((postData) => {
+      if (!currentWeekData) {
+        return postData
+      }
+      const merged = [...postData, ...currentWeekData]
+      const unique = Array.from(new Map(merged.map((v) => [v.id, v])).values())
+      return unique
+    })
+  }, [currentWeekData])
+
+  const updateSubject = useCallback(
+    async (newSubject: UpdateSubjectType) => {
+      try {
+        await putApi('/api/subjects', newSubject)
+        await mutate((postData) =>
+          postData?.map((v) => (v.id === newSubject.id ? { ...v, ...newSubject } : v))
+        )
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    [mutate]
+  )
+
+  // プリフェッチしてキャッシュを保持しておく
+  useGetApi<SubjectType[]>(`api/subjects?page=${currentWeekNum - 1}`)
+  useGetApi<SubjectType[]>(`api/subjects?page=${currentWeekNum + 1}`)
+
+  return { subjects, updateSubject }
+}
