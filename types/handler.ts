524d7c9bd9014a956c54b201566640a8b131bd1a
t-kabu <t.kabu.03@gmail.com>
2022-07-15 22:38:25 +0900

    fix(api routes): 冗長な処理を削除

--- types/handler.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { Session } from 'next-auth'
-
-export type HandlerArgs = {
-  req: NextApiRequest
-  res: NextApiResponse
-  session: Session
-}
